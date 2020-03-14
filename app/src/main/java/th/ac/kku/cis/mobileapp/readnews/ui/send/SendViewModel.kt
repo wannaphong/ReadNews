014b62b6bbd137eddf4c5b938880e14657c93540
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-03-14 16:12:09 +0700

    Add first file

---
+++ app/src/main/java/th/ac/kku/cis/mobileapp/readnews/ui/send/SendViewModel.kt
@@ -0,0 +1,13 @@
+package th.ac.kku.cis.mobileapp.readnews.ui.send
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SendViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is send Fragment"
+    }
+    val text: LiveData<String> = _text
+}
